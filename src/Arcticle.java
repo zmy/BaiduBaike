8f27f5062545bb38ec405317ff8f0aeec648fdf6
zejiachen <zejaichen@live.cn>
2013-06-30 00:14:42 +0800

    把不同类别的文件切分出来

--- src/Arcticle.java
+++ src/Arcticle.java
@@ -6,4 +6,8 @@ public class Arcticle {
 	String root;
 	String infobox;
 	String abstact;
+	String link;
+	public String toString(){
+		return index+"\n"+name+"\n"+catogory+"\n"+infobox+"\n"+abstact+"\n"+link+"\n";
+	}
 }
