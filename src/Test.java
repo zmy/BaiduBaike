a6773f8f999a47f07b79ac2927099224ce368b12
zejiachen <zejaichen@live.cn>
2013-06-29 21:31:35 +0800

    分词测试代码

---
+++ src/Test.java
@@ -0,0 +1,24 @@
+  
+import java.io.*; 
+import org.apache.lucene.analysis.TokenStream;  
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;  
+import org.wltea.analyzer.lucene.IKAnalyzer;  
+  
+public class Test {  
+	public static void main(String[] args) throws Exception{
+        String text="最近朋友都推荐使用github管理自己的项目,而且免费用户可以有5个仓库,恰好我也想了解下git,借此机会学习一下";  
+        IKAnalyzer analyzer=new IKAnalyzer(true);     
+        analyzer.setUseSmart(true);
+        StringReader reader=new StringReader(text);  
+        //�ִ�  
+        TokenStream ts=analyzer.tokenStream("", reader);  
+        CharTermAttribute term=ts.getAttribute(CharTermAttribute.class);  
+        //����ִ����  
+        while(ts.incrementToken()){  
+            System.out.print(term.toString()+"|");  
+        }  
+        reader.close();  
+        System.out.println(); 
+    }  
+  
+}  
